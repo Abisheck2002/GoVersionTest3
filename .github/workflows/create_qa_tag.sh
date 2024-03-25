339a0a2bd0b7c28a13f99d9c4271a1a5ce0cf15c
Abisheck26 <abisheckgnanavel@gmail.com>
2024-03-25 13:41:54 +0530

    changed sh

--- .github/workflows/create_qa_tag.sh
+++ .github/workflows/create_qa_tag.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Define the base version number
-BASE_VERSION="0.0.1"
+# Check if the current branch is QA
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" == "QA" ]; then
+    # Get the latest tag version
+    latest_tag=$(git describe --tags --abbrev=0)
 
-# Get the latest tag in the repository
-latest_tag=$(git describe --tags --abbrev=0)
+    # Extract the version number from the latest tag
+    latest_version=$(echo "$latest_tag" | grep -oP '(?<=0\.0\.)\d+')
 
-# Extract the version number from the latest tag
-latest_version=$(echo "$latest_tag" | grep -oP '(?<=0\.0\.)\d+')
+    # Increment the version number
+    next_version=$((latest_version + 1))
 
-# Increment the version number
-next_version=$((latest_version + 1))
+    # Create the new tag
+    new_tag="V0.0.${next_version}_QA"
+    git tag "$new_tag"
 
-# Create the new tag
-new_tag="V.0.0.${next_version}_QA"
-git tag "$new_tag"
+    # Push the new tag to the remote repository
+    git push origin "$new_tag"
 
-# Push the new tag to remote repository
-git push origin "$new_tag"
-
-echo "New tag created: $new_tag"
+    echo "New tag created: $new_tag"
+fi
